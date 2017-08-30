96b53a601a42d5c4127054cab2463afcd6ef528a
sathishkumar-sukumar <sathish.thangam@yahoo.com>
2017-08-30 13:05:55 +0530

    Update HelloWorld.java
    Change to Payments Team

--- src/radial/HelloWorld.java
+++ src/radial/HelloWorld.java
@@ -2,6 +2,6 @@ package radial;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Radial !!!");
+        System.out.println("Hello Payments Team !!!");
     }
 }
